d105e3fc77a7052543702d5e146d4daf494cfbda
Fayo08 <oyendefavour12@gmail.com>
2024-11-28 13:52:51 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,59 @@
+# guess = 5
+# user = int(input("Please type in a number: " ))
+# print(guess == user)
+
+
+# x = "hey"
+# y = "friend"
+# z = "there"
+
+# print(x)
+
+# x = "hello"
+# print(type(x))
+# print(x)
+
+# x = 1
+# print(type(x))
+# print(x)
+
+# my_array = [6, 12, 18, 4, 20, 25, 60]
+# my_array.append(15)
+# print(my_array)
+
+
+# my_array = [6, 12, 18, 4, 20, 25, 60]
+# print(my_array)
+# print(my_array[2:5])
+
+# my_array = [6, 12, 18, 4, 9]
+# my_array.pop()
+# print(my_array)
+
+# array = [5, 2, 6, 7]
+# element = 9
+# array.append(element)
+# print(array)
+
+# array = [5, 2, 6, 7]
+# array.pop(0)
+# print(array)
+
+# array = [5, 2, 6, 7]
+# print(array[3])
+
+people = ["Sundar", "Tim Apple", "Bill", "Mark"]
+
+numbers = {
+    "Bill": "613-555-0166",
+    "Ted": "111-222-3333",
+    "Jill": "234-565-4321",
+    "Jack": "876-545-6789",
+    "Mark": "777-423-1234",
+    "Jeff": "1 877-586-3230",
+    "Karen": "1 800-555-1221"
+}
+
+print(numbers["Jill"])
+print(type(numbers["Jill"]))
+print(numbers[people[2]])
